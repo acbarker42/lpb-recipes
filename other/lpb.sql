3ad202bc1316ddb5fd456bed928938f4283dd1bf
acbarker42 <autumn.c.barker@gmail.com>
2017-03-08 19:04:17 -0600

    Sql commands updated

--- other/lpb.sql
+++ other/lpb.sql
@@ -23,3 +23,20 @@ Phone Char(30) NOT NULL,
 City Char(30) NOT NULL,
 CONSTRAINT USER_PK PRIMARY KEY (UserID)
 );
+
+CREATE TABLE author (
+id Int AUTO_INCREMENT NOT NULL,
+first_name Char(30) NOT NULL,
+last_name Char(30) NOT NULL,
+website Char(30) NOT NULL,
+description Char(255)  NOT NULL,
+notes Char(255) NULL,
+CONSTRAINT AUTHOR_PK PRIMARY KEY (id)
+);
+
+INSERT INTO `author` VALUES 
+	(1,'David','Adams','david@luv2code.com', 'description', 'notes'),
+	(2,'John','Doe','john@luv2code.com', 'description', 'notes'),
+	(3,'Ajay','Rao','ajay@luv2code.com', 'description', 'notes'),
+	(4,'Mary','Public','mary@luv2code.com', 'description', 'notes'),
+	(5,'Maxwell','Dixon','max@luv2code.com', 'description', 'notes');
